59fe24f292cd12cf4ee59e1bf6a9c32bca52ceec
Enot <natasha.cat982404@gmail.com>
2020-04-20 14:51:21 +0300

    First

---
+++ src/MyInfo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './MyInfo.css';
+
+function MyInfo() {
+    return (
+      <div className="main">
+        <h1>Natasha</h1>
+        <p>smth about me</p>
+        <p>Place i want to visit:</p>
+        <ul>
+          <li>country 1</li>
+          <li>country 2</li>
+          <li>country 3</li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default MyInfo;
